638e0a2bc9fd6477ca7ed0e04f1e1f2183b9f175
BElannaP <xo.gamergirl@gmail.com>
2015-09-30 22:45:44 +0100

    made first blog post and got post working

---
+++ js/javascript.js
@@ -0,0 +1,20 @@
+var music1 = document.getElementById('music1');
+var music2 = document.getElementById('music2')
+var playbutton = document.getElementById('playButton');
+var pausebutton = document.getElementById('pauseButton');
+
+playbutton.addEventListener("click", function(){
+    playAudio(music2);
+});
+
+function playAudio(element, buttonel) {
+	if (element.paused) {
+		element.play();
+    playbutton.className = 'hide';
+    pausebutton.className = 'show';
+	} else { 
+		element.pause();
+    playbutton.className = 'show';
+    pausebutton.className = 'hide';
+	}
+}
