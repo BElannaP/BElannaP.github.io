3315055bec85dd5109ffad12fd0f8a581fd23d9d
BElannaP <xo.gamergirl@gmail.com>
2015-10-26 21:46:11 +0100

    started work on game

---
+++ theTraveler/javascript.js
@@ -0,0 +1,9 @@
+var nightmar = document.body.querySelector('p');
+
+var smile = document.body.querySelector('img');
+
+function showSmiley(){
+    smile.style.opacity = "1";
+}
+nightmar.addEventListener("animationend", showSmiley,false);
+nightmar.style.animation = "type 2s steps(60, end)";
